1b5eda911a49d2dcecedd0f458abb1417e1f9173
Katlego <61965233+Katlegokt38@users.noreply.github.com>
2020-08-06 10:44:49 +0200

    Updated database details

--- LocalDB/phpmyadminDB/cos301.sql
+++ LocalDB/phpmyadminDB/cos301.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2020 at 10:19 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Generation Time: Aug 06, 2020 at 10:37 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,26 +28,41 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `distance` (
-  `Rooms` varchar(100) NOT NULL,
-  `Texas` varchar(100) NOT NULL,
-  `Colorado` varchar(100) NOT NULL,
-  `NewJersey` varchar(100) NOT NULL,
-  `Mississippi` varchar(100) NOT NULL,
-  `NewYork` varchar(100) NOT NULL,
-  `California` varchar(100) NOT NULL,
-  `Florida` varchar(100) NOT NULL,
-  `Pennsylvania` varchar(100) NOT NULL,
-  `Georgia` varchar(100) NOT NULL,
-  `Tennessee` varchar(100) NOT NULL,
-  `Washington` varchar(100) NOT NULL
+  `Rooms` varchar(255) NOT NULL,
+  `Texas` int(3) NOT NULL,
+  `Colorado` int(3) NOT NULL,
+  `Mississippi` int(3) NOT NULL,
+  `NewJersey` int(3) NOT NULL,
+  `NewYork` int(3) NOT NULL,
+  `California` int(3) NOT NULL,
+  `Florida` int(3) NOT NULL,
+  `Pennsylvania` int(3) NOT NULL,
+  `Georgia` int(3) NOT NULL,
+  `Tennessee` int(3) NOT NULL,
+  `Washington` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `distance`
 --
 
-INSERT INTO `distance` (`Rooms`, `Texas`, `Colorado`, `NewJersey`, `Mississippi`, `NewYork`, `California`, `Florida`, `Pennsylvania`, `Georgia`, `Tennessee`, `Washington`) VALUES
-('1A2', '23', '21', '22', '142', '12', '22', '12', '2', '3', '142', '9');
+INSERT INTO `distance` (`Rooms`, `Texas`, `Colorado`, `Mississippi`, `NewJersey`, `NewYork`, `California`, `Florida`, `Pennsylvania`, `Georgia`, `Tennessee`, `Washington`) VALUES
+('1A1', 37, 42, 48, 51, 74, 82, 88, 81, 86, 92, 95),
+('1A2', 18, 24, 28, 32, 81, 89, 95, 88, 93, 99, 102),
+('1C2', 21, 17, 21, 25, 89, 97, 103, 96, 101, 107, 110),
+('1D1', 49, 54, 60, 63, 86, 94, 100, 93, 98, 104, 107),
+('1F2', 27, 21, 20, 15, 101, 109, 115, 108, 113, 119, 122),
+('1H2', 21, 16, 20, 24, 94, 101, 107, 101, 105, 111, 114),
+('1I2', 25, 20, 20, 16, 101, 108, 114, 108, 112, 118, 121),
+('1J1', 46, 50, 56, 59, 83, 90, 96, 90, 94, 100, 103),
+('1K1', 54, 58, 64, 67, 91, 98, 104, 98, 102, 108, 111),
+('1L1', 61, 65, 71, 74, 98, 105, 111, 105, 109, 115, 118),
+('1L2', 21, 16, 16, 12, 105, 112, 118, 112, 116, 122, 125),
+('1M1', 50, 54, 60, 63, 87, 94, 100, 94, 98, 104, 107),
+('1M2', 9, 15, 19, 23, 94, 101, 107, 101, 105, 111, 114),
+('1P1', 48, 53, 59, 62, 85, 93, 99, 92, 97, 103, 106),
+('1R1', 56, 61, 67, 70, 93, 101, 107, 100, 105, 111, 114),
+('1U1', 68, 73, 79, 82, 105, 113, 119, 112, 117, 123, 126);
 
 -- --------------------------------------------------------
 
@@ -57,12 +71,12 @@ INSERT INTO `distance` (`Rooms`, `Texas`, `Colorado`, `NewJersey`, `Mississippi`
 --
 
 CREATE TABLE `employeedetails` (
-  `EmpEmail` varchar(100) NOT NULL,
-  `FirstName` varchar(100) NOT NULL,
-  `LastName` varchar(100) NOT NULL,
-  `EmpPassword` varchar(100) NOT NULL,
-  `isAdmin` varchar(100) NOT NULL,
-  `LocationID` varchar(100) NOT NULL
+  `EmpEmail` varchar(255) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `EmpPassword` varchar(255) NOT NULL,
+  `isAdmin` bit(1) NOT NULL,
+  `LocationID` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -70,7 +84,23 @@ CREATE TABLE `employeedetails` (
 --
 
 INSERT INTO `employeedetails` (`EmpEmail`, `FirstName`, `LastName`, `EmpPassword`, `isAdmin`, `LocationID`) VALUES
-('fdgfd@e.com', 'Emma', 'joburg', '142', 'false', '1A2');
+('AdeleV@teamthreshold.onmicrosoft.com', 'Adele', 'Vance', 'password', b'1', '1A1'),
+('AlexW@teamthreshold.onmicrosoft.com', 'Alex ', 'Wilber', 'password', b'0', '1D1'),
+('COS301@teamthreshold.onmicrosoft.com', 'Team', 'Threshold', 'Thresh#301', b'0', '1M1'),
+('DiegoS@teamthreshold.onmicrosoft.com', 'Diego', 'Siciliani', 'password', b'0', '1P1'),
+('GradyA@teamthreshold.onmicrosoft.com', 'Grady', 'Archie', 'password', b'0', '1U1'),
+('HenriettaM@teamthreshold.onmicrosoft.com', 'Henrietta', 'Mueller', 'password', b'0', '1K1'),
+('IsaiahL@teamthreshold.onmicrosoft.com', 'Isaiah', 'Langer', 'password', b'0', '1R1'),
+('JohannaL@teamthreshold.onmicrosoft.com', 'Johanna', 'Lorenz', 'password', b'0', '1L1'),
+('JoniS@teamthreshold.onmicrosoft.com', 'Joni', 'Sherman', 'password', b'0', '1J1'),
+('LeeG@teamthreshold.onmicrosoft.com', 'Lee', 'Gu', 'password', b'0', '1A2'),
+('LidiaH@teamthreshold.onmicrosoft.com', 'Lidia', 'Holloway', 'password', b'0', '1C2'),
+('LynneR@teamthreshold.onmicrosoft.com', 'Lynne', 'Robbins', 'password', b'0', '1F2'),
+('MeganB@teamthreshold.onmicrosoft.com', 'Megan', 'Bowen', 'password', b'0', '1I2'),
+('MiriamG@teamthreshold.onmicrosoft.com', 'Miriam', 'Graham', 'password', b'0', '1F2'),
+('NestorW@teamthreshold.onmicrosoft.com', 'Nestor', 'Wilke', 'password', b'0', '1M2'),
+('PattiF@teamthreshold.onmicrosoft.com', 'Patti', 'Fernandez', 'password', b'0', '1H2'),
+('PradeepG@teamthreshold.onmicrosoft.com', 'PradeepG', 'Gupta', 'password', b'1', '1L2');
 
 -- --------------------------------------------------------
 
@@ -79,30 +109,34 @@ INSERT INTO `employeedetails` (`EmpEmail`, `FirstName`, `LastName`, `EmpPassword
 --
 
 CREATE TABLE `floorplan` (
-  `RoomID` varchar(3) NOT NULL,
-  `RoomName` varchar(100) NOT NULL,
-  `FloorNumber` int(100) NOT NULL,
-  `maxSeats` int(100) NOT NULL,
-  `Amenity` varchar(150) NOT NULL,
-  `Building` tinyint(1) NOT NULL,
-  `Whiteboard` tinyint(1) NOT NULL,
-  `Projector` tinyint(1) NOT NULL,
-  `Monitor` tinyint(1) NOT NULL,
-  `isExternal` tinyint(1) NOT NULL,
-  `isAvailable` tinyint(1) NOT NULL
+  `RoomID` varchar(255) NOT NULL,
+  `RoomName` varchar(255) NOT NULL,
+  `FloorNumber` int(2) NOT NULL,
+  `maxSeats` int(2) NOT NULL,
+  `Amenity` varchar(255) NOT NULL,
+  `isExternal` bit(1) NOT NULL,
+  `Building` int(2) NOT NULL,
+  `Whiteboard` bit(1) NOT NULL,
+  `Projector` bit(1) NOT NULL,
+  `Monitor` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `floorplan`
 --
 
-INSERT INTO `floorplan` (`RoomID`, `RoomName`, `FloorNumber`, `maxSeats`, `Amenity`, `Building`, `Whiteboard`, `Projector`, `Monitor`, `isExternal`, `isAvailable`) VALUES
-('1D4', 'Texas', 2, 12, 'Whiteboard,Projector', 1, 0, 0, 0, 0, 0),
-('1J8', 'collarado', 2, 12, 'Whiteboard,Projector', 1, 0, 0, 0, 0, 0),
-('1mp', 'collarado', 2, 12, 'Whiteboard,Projector', 1, 0, 0, 0, 0, 0),
-('1N8', 'collarado', 2, 12, 'Whiteboard,Projector', 1, 0, 0, 0, 0, 0),
-('bjA', 'Texas', 2, 12, 'Whiteboard,Projector', 1, 0, 0, 0, 0, 0),
-('und', 'undefined', 0, 0, 'undefined', 0, 0, 0, 0, 0, 0);
+INSERT INTO `floorplan` (`RoomID`, `RoomName`, `FloorNumber`, `maxSeats`, `Amenity`, `isExternal`, `Building`, `Whiteboard`, `Projector`, `Monitor`) VALUES
+('1P2', 'Texas', 2, 10, 'Whiteboard,Projector', b'0', 1, b'1', b'1', b'0'),
+('1Q2', 'Colorado', 2, 6, 'Whiteboard', b'0', 1, b'1', b'0', b'0'),
+('1R2', 'Mississippi', 2, 6, 'Projector', b'0', 1, b'0', b'1', b'0'),
+('1S2', 'NewJersey', 2, 6, 'Whiteboard,Monitor', b'0', 1, b'1', b'0', b'1'),
+('2P1', 'NewYork', 1, 10, 'Whiteboard,Projector', b'0', 2, b'1', b'1', b'0'),
+('2P2', 'Pennsylvania', 2, 10, 'Whiteboard,Projector', b'0', 2, b'1', b'1', b'0'),
+('2Q1', 'California', 1, 10, 'Whiteboard,Monitor', b'0', 2, b'1', b'0', b'1'),
+('2Q2', 'Georgia', 2, 6, 'Whiteboard', b'0', 2, b'1', b'0', b'0'),
+('2R1', 'Florida', 1, 10, 'Whiteboard,Monitor', b'0', 2, b'1', b'0', b'1'),
+('2R2', 'Tennessee', 2, 6, 'Monitor', b'0', 2, b'0', b'0', b'1'),
+('2S2', 'Washington', 2, 6, 'Whiteboard,Projector', b'0', 2, b'1', b'1', b'0');
 
 -- --------------------------------------------------------
 
