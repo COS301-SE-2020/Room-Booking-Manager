c57eaf68c28ac5de84aba11b79b9a93bb780d849
Abu <55980260+Abu-22@users.noreply.github.com>
2020-08-20 10:10:42 +0200

    Exported some modules and added unit tests
    Authentication, API call and Amenity AI Unit test

--- Updated codebase/rbm.js
+++ Updated codebase/rbm.js
@@ -82,9 +82,9 @@ async function getEventdetails(accessToken) {
     return new Promise((resolve, reject) => {
         const subscription = {
             changeType: "created",
-            notificationUrl: "https://e82da2037efb.ngrok.io/webhook",
+            notificationUrl: "https://e749277a82e0.ngrok.io/webhook",
             resource: "users/b84f0efb-8f72-4604-837d-7ce7ca57fdd4/events", // Subscribe to each employees events
-            expirationDateTime: "2020-08-18T20:30:45.9356913Z",
+            expirationDateTime: "2020-08-20T08:30:45.9356913Z",
             clientState: "secretClientValue",
             latestSupportedTlsVersion: "v1_2",
         };
